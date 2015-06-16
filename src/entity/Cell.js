0f7214439a39c470e3efb55a245315339b138ed0
yrsegal <yrsegal@gmail.com>
2015-06-15 23:54:23 -0400

    Sigh, bots
    This reverts commit 5455c53d9b645ec96aaef0e2b6141f1b571734ac.
    closes #4

--- src/entity/Cell.js
+++ src/entity/Cell.js
@@ -296,7 +296,18 @@ Cell.prototype.calcMovePhys = function(gameServer) {
     var list = this.predatorsAlongPath(A, B, gameServer);
     if (list.length) {
         var check = this.closestPredator(A, list);
-        this.position = check.position;
+
+        // Consume effect
+        if (!check.getType()) {
+            this.onConsume(check,gameServer);
+        }
+        else if (check.getType() == 2) {
+            check.feed(this, gameServer);
+        }
+
+        // Remove cell
+        this.setKiller(check);
+        gameServer.removeNode(this);
     }
 };
 
